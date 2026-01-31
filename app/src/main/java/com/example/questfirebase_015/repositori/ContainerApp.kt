c97b50a8ccad3893f44fd46ef0d552c08476b31e
haris49 <hariis.459@gmail.com>
2026-01-31 13:10:29 +0700

    tambah DefaultContainerApp

--- app/src/main/java/com/example/questfirebase_015/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questfirebase_015/repositori/ContainerApp.kt
@@ -3,3 +3,9 @@ package com.example.questfirebase_015.repositori
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
+
+class DefaultContainerApp : com.example.myfirebase.repositori.ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
