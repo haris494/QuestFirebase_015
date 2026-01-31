ad6b19e08a62de7ef877345f1d1507ede78f3357
haris49 <hariis.459@gmail.com>
2026-01-31 12:57:23 +0700

    add siswa data class

---
+++ app/src/main/java/com/example/questfirebase_015/modeldata/Siswa.kt
@@ -0,0 +1,8 @@
+package com.example.questfirebase_015.modeldata
+
+data class Siswa(
+    val id: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
