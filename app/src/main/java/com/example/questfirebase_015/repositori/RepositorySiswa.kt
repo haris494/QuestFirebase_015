d6392e2366a8f54fb7a83989f6c847f150c475d0
haris49 <hariis.459@gmail.com>
2026-01-31 13:16:58 +0700

    tambah edit data siswa

--- app/src/main/java/com/example/questfirebase_015/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/questfirebase_015/repositori/RepositorySiswa.kt
@@ -56,3 +56,16 @@ class FirebaseRepositorySiswa : RepositorySiswa {
             null
         }
     }
+
+    override suspend fun editSatuSiswa(id: String, siswa: Siswa) {
+        val docQuery = collection.whereEqualTo("id", id).get().await()
+        val docId = docQuery.documents.firstOrNull()?.id ?: return
+        collection.document(docId).set(
+            mapOf(
+                "id" to siswa.id,
+                "nama" to siswa.nama,
+                "alamat" to siswa.alamat,
+                "telpon" to siswa.telpon
+            )
+        ).await()
+    }
