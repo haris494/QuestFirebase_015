8c6486eb6b562c9ec2abf28abfec9a70a815cedd
haris49 <hariis.459@gmail.com>
2026-01-31 13:34:23 +0700

    buat EditSiswaScreen

---
+++ app/src/main/java/com/example/questfirebase_015/view/HalmanEdit.kt
@@ -0,0 +1,14 @@
+package com.example.questfirebase_015.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
