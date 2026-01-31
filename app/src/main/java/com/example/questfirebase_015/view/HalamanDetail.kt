2b797b5d184d173e49ea7e116e20bedbd6afc0b8
haris49 <hariis.459@gmail.com>
2026-01-31 13:32:17 +0700

    tambah tombol konfirmasi hapus

--- app/src/main/java/com/example/questfirebase_015/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questfirebase_015/view/HalamanDetail.kt
@@ -199,4 +199,10 @@ private fun DeleteConfirmationDialog(
                 Text(stringResource(R.string.no))
             }
         },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(stringResource(R.string.yes))
+            }
+        })
+}
 
