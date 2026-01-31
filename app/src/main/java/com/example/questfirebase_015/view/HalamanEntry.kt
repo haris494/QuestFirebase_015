47b1499082df5a91267c8edca94cf272beeb0359
haris49 <hariis.459@gmail.com>
2026-01-31 13:36:45 +0700

    tambah aksi simpan siswa

--- app/src/main/java/com/example/questfirebase_015/view/HalamanEntry.kt
+++ app/src/main/java/com/example/questfirebase_015/view/HalamanEntry.kt
@@ -1,5 +1,9 @@
 package com.example.questfirebase_015.view
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -8,6 +12,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -28,3 +33,20 @@ fun EntrySiswaScreen(
                 scrollBehavior = scrollBehavior
             )
         }) { innerPadding ->
+
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.addSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
