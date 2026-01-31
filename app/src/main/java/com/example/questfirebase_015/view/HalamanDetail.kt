19b4684694efbdcca5c96c0014fa56f11815fd05
haris49 <hariis.459@gmail.com>
2026-01-31 13:27:04 +0700

    tambah halaman detail siswa

---
+++ app/src/main/java/com/example/questfirebase_015/view/HalamanDetail.kt
@@ -0,0 +1,24 @@
+package com.example.questfirebase_015.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (String) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetail.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
